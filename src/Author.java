0be7771211e8c163e727220769d82ef25b195f23
Stephanie Velazquez <svelazquez@Stephanies-MacBook-Air.local>
2018-09-27 14:34:29 -0500

    finished exercise

--- src/Author.java
+++ src/Author.java
@@ -6,11 +6,11 @@ import java.util.List;
 
 public class Author extends Person {
 
-    private List books;
+    private List<String> books;
 
     public Author(String firstName, String lastName) {
         super(firstName, lastName);
-        books = new ArrayList();
+        books = new ArrayList<>();
     }
 
     /**
